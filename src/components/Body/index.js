93300ce1a3024665b2e9c779451cf8e274fb3601
Arian Vivo Alesso <arian_v_alesso@hotmail.com>
2023-03-28 00:07:25 -0300

    fix body

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -4,9 +4,7 @@ import './styles.css'
 
 const Body = () => {
     return (
-        <body className="">
-            <h1 className="title">Insumos y componentes de máquinas arcade</h1>
-        </body>
+        <body />
     )    
 }
 
